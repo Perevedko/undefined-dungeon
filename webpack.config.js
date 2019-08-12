f935809b5ba1cab15cf478378f1fd486a89c4415
Alien Wave <alienwave@iMac.local>
2019-08-12 09:31:16 +0300

    edit package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,16 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        'html': './client/index.html',
         'css': './client/style.css'
     },
 
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
-    
+
     module: {
         rules: [
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader'
-                }
-            },
-
             {
                 test: /\.сss$/,
                 use: 'raw-loader'
