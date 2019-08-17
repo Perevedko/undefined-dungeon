902cbbe0ac848c53453b727b2391a58f7aa3b80e
Alien Wave <istominmartin@gmail.com>
2019-08-17 20:47:47 +0300

    edit comments

--- client/sokoban.js
+++ client/sokoban.js
@@ -11,15 +11,15 @@ const IMAGES_PATHS = Object.values(IMAGES);
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let LOADED_ASSETS = {};
+function requireAsset(link) { // функция прогрузки изображений
+    let LOADED_ASSETS = {};
 
-function requireAsset(link) {
     new Promise(resolve => {
-        if (LOADED_ASSETS.hasOwnProperty(link)) { // если изображение прогружено
+        if (LOADED_ASSETS.hasOwnProperty(link)) { // если изображение прогружено, то
             resolve(LOADED_ASSETS[link]); // отправляет изоюражение во внешний мир
         }
 
-        else { // если изображение ещё не загружено
+        else { // если изображение ещё не загружено, то оно прогружается
             const image = new Image();
 
             image.addEventListener('load', () => {
@@ -33,37 +33,37 @@ function requireAsset(link) {
 }
 
 function draw(imageUrl, tileX, tileY) {
-    requireAsset(imageUrl).then(image =>
+    requireAsset(imageUrl).then(image => // запрашивает изображение из функции requireAsset
         ctx.drawImage(image, tileX * TILE_SIZE, tileY * TILE_SIZE, TILE_SIZE, TILE_SIZE));
 }
 
 function drawPlayer(coords) {
     const x = coords.x;
     const y = coords.y;
-    draw(IMAGES.player, x, y);
+    draw(IMAGES.player, x, y); // рисует персонажа из шаблона draw
 }
 
-const symbolToImageMapping = {
+const symbolToImageMapping = { // символы массива в изображения
     'x': IMAGES.wall,
     '.': IMAGES.floor,
     'h': IMAGES.floor
 };
 
-function drawMap(tileMap) {
-    tileMap.forEach((row, j), function () {
+function drawMap(tileMap) { // нихрена не понимаю, почему столько foreach
+    tileMap.forEach((row, j), function () { // где мои for, их проще воспринимать
         row.forEach((tile, i), function () {
-            draw(symbolToImageMapping[tile], i, j)
+            draw(symbolToImageMapping[tile], i, j);
         });
     });
 }
 
-function request(url, data) {
+function request(url, data) { // функция запроса к апишке 
     fetch(url, data).then(result => result.json());
 }
 
 function startGame() {
     Promise.all(IMAGES_PATHS.map(requireAsset)).then(() => {
-        request('/api/game/new').then(gameState => {
+        request('/api/game/new').then(gameState => { // запрос к новой игре
             console.log(gameState);
             const id = gameState.id;
             drawMap(gameState.board);
